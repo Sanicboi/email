2adb5e093c6f4614530a788f7671f9c9370d63dd
sanicboi <alexandrzezekalo@gmail.com>
2025-07-02 14:14:23 +0300

    all doen

--- src/index.ts
+++ src/index.ts
@@ -2,55 +2,136 @@ import { AppDataSource } from "./data-source";
 import { Agent, AgentInputItem, run } from "@openai/agents";
 import "dotenv/config";
 import TelegramBot from "node-telegram-bot-api";
-import fs from "fs";
+import fs, { read, write } from "fs";
 import path from "path";
+import { z } from "zod";
+import axios, { AxiosResponse } from "axios";
+
+/**
+ * AI configuration object
+ */
+interface IConfig {
+  /**
+   * The path to knowledge base
+   */
+  kb: string;
+  /**
+   * The prompt
+   */
+  prompt: string;
+  /**
+   * The model
+   */
+  model: string;
+  /**
+   * The first message
+   */
+  firstMessage: string;
+}
+
 
 
 let thread: AgentInputItem[] = [];
-let waiter: "none" | "db" | "prompt" | "model" | "firstMessage" = "none";
-let prompt = fs.readFileSync(path.join(process.cwd(), 'data', "prompt.txt"), "utf-8");
-let db = fs.readFileSync(path.join(process.cwd(), 'data', "db.txt"), "utf-8");
-let firstMessage = fs.readFileSync(path.join(process.cwd(), 'data', 'firstMessage.txt'), 'utf-8');
-let model = fs.readFileSync(path.join(process.cwd(), 'data', 'model.txt'), 'utf-8');
+let waiter: "none" | keyof IConfig = "none";
+let config: IConfig = JSON.parse(
+  fs.readFileSync(path.join(process.cwd(), "ai.config.json"), "utf-8")
+);
 
-// AppDataSource.initialize().then(async () => {
+const readKB = (): string => {
+  if (!config.kb) throw new Error("No kb!");
+  return fs.readFileSync(
+    path.join(process.cwd(), "data", config.kb),
+    "utf-8"
+  )
+}
+
+const getWriterInstructions = (): string => {
+  let result: string = `${config.prompt}`
+  if (config.kb) result += `${config.prompt}\n\n\nБаза знаний:${readKB()}`;
+  return result;
+};
+
+const getValidatorInstructions = (): string => {
+  let result: string = `Ты - менеджер по продажам. Тебе будет дана цепочка писем. Убедись, что последнее сообщение соответствует требованиям, указанным в Базе знаний. В ответе укажи соответсвует ли оно и дай комментарий (почему)`;
+  if (config.kb) result += `${config.prompt}\n\n\nБаза знаний:${readKB()}`;
+  return result;
+}
+
+const getDeterminerInstructions = (): string => {
+  return `Ты - менеджер по продажам. Тебе будет дана цепочка писем. Определи, нужно ли отвечать на последнее электронное письмо, и объясни, почему. В ответе укажи результат определения (answer) и комментарий (comment)`
+}
 const bot = new TelegramBot(process.env.TG_TOKEN!, {
   polling: true,
 });
 
 bot.setMyCommands([
   {
-    command: 'start',
-    description: 'Начать новый диалог'
+    command: "start",
+    description: "Начать новый диалог",
   },
   {
-    command: 'prompt',
-    description: 'Изменить промпт'
+    command: "info",
+    description: "Информация",
   },
   {
-    command: 'db',
-    description: 'Изменить Базу знаний (текстово)'
+    command: "prompt",
+    description: "Изменить промпт",
   },
   {
-    command: 'model',
-    description: 'Изменить модель'
+    command: "kb",
+    description: "Изменить Базу знаний (текстовый файл)",
   },
   {
-    command: 'first',
-    description: 'Изменить первое сообщение'
-  }
-])
+    command: "model",
+    description: "Изменить модель",
+  },
+  {
+    command: "first",
+    description: "Изменить первое сообщение",
+  },
+  {
+    command: "info",
+    description: "Информация",
+  },
+]);
 
-const agent = new Agent({
+const writer = new Agent({
   name: "МОП",
-  model,
-  instructions: `${prompt}\n\nДополнительные данные:${db}`,
+  model: config.model,
+  instructions: getWriterInstructions,
+});
+
+const validator = new Agent({
+  name: "Валидатор",
+  model: config.model,
+  instructions: getValidatorInstructions,
+  outputType: z.object({
+    ok: z.boolean(),
+    comment: z.string(),
+  }),
+});
+
+const determiner = new Agent({
+  name: "Определитель",
+  model: config.model,
+  instructions: getDeterminerInstructions,
+  outputType: z.object({
+    answer: z.boolean(),
+    comment: z.string(),
+  }),
+});
+
+bot.onText(/\/info/, async (msg) => {
+  await bot.sendMessage(
+    msg.chat.id,
+    "Демо версия рассыльщика. Определитель работает полностью, валидатор не мешает не отправлять сообщение. БЗ на данный момент исключительно в формате .txt (поскольку сейчас она подгружается в диалог как сообщение, а не векторно, т.к. нет возможности прочитать доки библиотеки агентов опенаи, ), изменю в след версии на пдф"
+  );
 });
 
 bot.onText(/\/start/, async (msg) => {
   thread = [];
-  waiter = 'none';
-  await bot.sendMessage(msg.chat.id, firstMessage);
+  waiter = "none";
+  await bot.sendMessage(msg.chat.id, config.firstMessage);
 });
 
 bot.onText(/./, async (msg) => {
@@ -63,7 +144,7 @@ bot.onText(/./, async (msg) => {
         content: [
           {
             type: "output_text",
-            text: firstMessage,
+            text: config.firstMessage,
           },
         ],
       });
@@ -78,39 +159,71 @@ bot.onText(/./, async (msg) => {
         },
       ],
     });
-    const result = await run(agent, thread);
 
+    await bot.sendMessage(msg.chat.id, 'Запускаю определителя...');
+    const determinationResult = await run(determiner, thread);
+    await bot.sendMessage(msg.chat.id, `Результат определителя: ${determinationResult.finalOutput?.answer}\nКомментарий:${determinationResult.finalOutput?.comment}`);
+    if (!determinationResult.finalOutput?.answer) return;
+    await bot.sendMessage(msg.chat.id, 'Запускаю писателя')
+    
+    const result = await run(writer, thread);
     thread = result.history;
     await bot.sendMessage(msg.from!.id, result.finalOutput!);
-  } else {
-    fs.writeFileSync(path.join(process.cwd(), 'data', `${waiter}.txt`), msg.text!, 'utf-8');
-    waiter = 'none';
+
+    await bot.sendMessage(msg.chat.id, 'Запускаю валидатора...');
+    const validation = await run(validator, thread);
+    await bot.sendMessage(msg.chat.id, `Результат определителя: ${validation.finalOutput?.ok}\nКомментарий:${validation.finalOutput?.comment}`);
+
+  } else if (waiter != 'kb') {
+    config[waiter] = msg.text!;
+    fs.writeFileSync(path.join(process.cwd(), 'ai.config.json'), JSON.stringify(config), 'utf-8');
+    waiter = "none";
     thread = [];
-    await bot.sendMessage(msg.from!.id, 'Изменил данные и сбросил диалог');
+    await bot.sendMessage(msg.from!.id, "Изменил данные и сбросил диалог");
   }
 });
 
 bot.onText(/\/prompt/, async (msg) => {
   waiter = "prompt";
-  await bot.sendMessage(msg.chat.id, prompt);
   await bot.sendMessage(msg.chat.id, "Пришлите мне новый системный промпт");
 });
 
-bot.onText(/\/db/, async (msg) => {
-  waiter = "db";
-  await bot.sendMessage(msg.chat.id, db);
-  await bot.sendMessage(msg.chat.id, "Пришлите мне новую базу знаний");
+bot.onText(/\/kb/, async (msg) => {
+  waiter = "kb";
+  await bot.sendMessage(msg.chat.id, "Пришлите мне новую базу знаний (файлом .txt)");
 });
 
 bot.onText(/\/model/, async (msg) => {
-    waiter = 'model';
-    await bot.sendMessage(msg.chat.id, model);
-    await bot.sendMessage(msg.chat.id, 'Пришлите мне новую модель (в формате OpenAI для кода, примеры: gpt-4o\ngpt-4.1-nano\ngpt-4o-mini\ngpt-4.1\ngpt-4-turbo\nИ др.)')
+  waiter = "model";
+  await bot.sendMessage(
+    msg.chat.id,
+    "Пришлите мне новую модель (в формате OpenAI для кода, примеры: gpt-4o\ngpt-4.1-nano\ngpt-4o-mini\ngpt-4.1\ngpt-4-turbo\nИ др.)"
+  );
 });
 
 bot.onText(/\/first/, async (msg) => {
-    waiter = 'firstMessage';
-    await bot.sendMessage(msg.chat.id, firstMessage);
-    await bot.sendMessage(msg.chat.id, 'Пришлите мне новое первое сообщение');
+  waiter = "firstMessage";
+  await bot.sendMessage(msg.chat.id, "Пришлите мне новое первое сообщение");
+});
+
+bot.on('document', async (msg) => {
+  if (!msg.document) return;
+  if (waiter !== 'kb') return;
+  const url = await bot.getFileLink(msg.document.file_id);
+  if (path.extname(url) !== '.txt') {
+    await bot.sendMessage(msg.chat.id, 'Пришлите файл .txt!');
+    return;
+  }
+  const {data}: AxiosResponse<Buffer> = await axios.get(url, {
+    responseType: 'arraybuffer'
+  });
+
+  fs.writeFileSync(
+    path.join(process.cwd(), 'data', config.kb),
+    data,
+    'utf-8'
+  );
+  waiter = 'none';
+  thread = [];
+  await bot.sendMessage(msg.chat.id, 'База данных изменена и диалог сброшен');
 })
-// });
